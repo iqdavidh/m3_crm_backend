c2f7abb3fb311b04efe14f62d7c3a6f61167b4db
davidh <iq.davidh@gmail.com>
2019-10-04 09:53:52 -0500

    avance ya se tiene todos

--- src/model/Cliente/ModelCliente.js
+++ src/model/Cliente/ModelCliente.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
-const ClienteSchema = new Schema({
+
+const ModelCliente = new Schema({
   nombre: {type: String, require: true},
   apaterno: {type: String, require: true},
   amaterno: {type: String},
   tel: {type: String, trim: true},
   origen: {type: String, trim: true},
   email1: {type: String, trim: true},
+  id_usuario: {type: String},
   indicadores: {funelIndex: {type: Number, min: 1, max: 4}}
 });
 
-module.exports = mongoose.model('clientes', ClienteSchema);
+module.exports = mongoose.model('clientes', ModelCliente);
