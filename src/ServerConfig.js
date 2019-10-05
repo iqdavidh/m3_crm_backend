6dea27982621bd6e82adf5e7fe7b516ebc0b7247
davidh <iq.davidh@gmail.com>
2019-10-04 19:50:12 -0500

    los bosn para exportar

--- src/ServerConfig.js
+++ src/ServerConfig.js
@@ -3,18 +3,34 @@
 
 
 const ServerConfig = {
-  tipoServer: "development" ,
+  tipoServer: "aws" ,
   mongo: {
     urlServer: "mongodb://localhost:27017",
-    user: "davidh",
-    pass: "lalocal",
+    user: "iron",
+    pass: "iron",
   },
   back: {
     port:3004,
-    url: "http://localhost:3004"
+    url: "http://52.37.165.46:3004"
   },
   jwtSecret:"unsaltsegurode16characters"
 };
+
+
+//
+// const ServerConfig = {
+//   tipoServer: "development" ,
+//   mongo: {
+//     urlServer: "mongodb://localhost:27017",
+//     user: "davidh",
+//     pass: "lalocal",
+//   },
+//   back: {
+//     port:3004,
+//     url: "http://localhost:3004"
+//   },
+//   jwtSecret:"unsaltsegurode16characters"
+// };
 
 
 module.exports = ServerConfig;
